79b33bbdb80f3c91ddb408e0cdb0722742472916
Marco Aurelio Nissikawa Hisatomi <marcohisatomi@alunos.utfpr.edu.br>
2024-05-15 22:48:36 -0300

    adiciona classe para controle da interface gráfica

--- Game.java
+++ Game.java
@@ -1,20 +1,9 @@
 import java.util.ArrayList;
 
 public class Game {
-    private Frame frame;
-    private ArrayList<Entity> entities;
+    private GUI gui;
 
     public Game () {
-        this.frame = new Frame();
-        this.entities = new ArrayList<Entity>();
-
-        this.frame.setVisible(true);
-    }
-
-    public void addEntity(Entity newEntity) {
-        this.entities.add(newEntity);
-        this.frame.add(newEntity);
-
-        this.frame.setVisible(true);
+        this.gui = new GUI();
     }
 }
