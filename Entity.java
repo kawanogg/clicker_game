fb5f071dbf475a2c31074c2b6f099fc5a4cb144b
Marco Aurelio Nissikawa Hisatomi <marcohisatomi@alunos.utfpr.edu.br>
2024-05-16 22:24:42 -0300

    adiciona verificação de cliques no planeta

--- Entity.java
+++ Entity.java
@@ -1,24 +1,27 @@
 import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.io.*;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 
 public class Entity extends JPanel {
-    private BufferedImage image;
-    private int x, y, width, height;
+    protected Image image;
+    protected int x, y, width, height;
 
     public Entity(String imagePath, int x, int y, int width, int height) {
-        try {
-            image = ImageIO.read(new File(imagePath));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
         this.x = x;
         this.y = y;
         this.width = width;
         this.height = height;
+
+        try {
+            BufferedImage buffered_image = ImageIO.read(new File(imagePath));
+            image = buffered_image.getScaledInstance(width, height, Image.SCALE_DEFAULT);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }       
     }
 
     @Override
--- Planet.java
+++ Planet.java
@@ -1,5 +1,24 @@
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
 public class Planet extends Entity {
-    public Planet (String imagePath, int x, int y) {
-        super(imagePath, x, y);
+    public Planet (int x, int y, int width, int height) {
+        super("img/planet.png", x, y, width, height);
+
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed (MouseEvent e) {
+                int mouseX = e.getX();
+                int mouseY = e.getY();
+
+                System.out.printf("Mouse: (%d, %d)\n", mouseX, mouseY);
+
+                if (mouseX >= x && mouseX <= x + width && mouseY >= y && mouseY <= y + height) {
+                    System.out.println("Eu não sou um animal");
+                }
+            }
+        });
     }
+
+    
 }
