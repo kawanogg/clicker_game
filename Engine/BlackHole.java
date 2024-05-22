9d845fc709d2c08fbb636865e9a3bc6bdd4a2160
Marco Aurelio Nissikawa Hisatomi <marcohisatomi@alunos.utfpr.edu.br>
2024-05-22 20:58:27 -0300

    corrige imagem ao clicar

--- Engine/BlackHole.java
+++ Engine/BlackHole.java
@@ -1,15 +1,28 @@
 package Engine;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Image;
+import java.awt.Graphics;
+import javax.swing.Timer;
 
 public class BlackHole extends Entity {
 
-    Player player;
+    private Player player;
+    private Image image_clicked;
+    private int x_clicked;
+    private int y_clicked;
+
 
     public BlackHole (int x, int y, int width, int height, Player player) {
         super("img/black_hole.png", x, y, width, height);
 
         this.player = player;
+        this.x_clicked = x - 10;
+        this.y_clicked = y - 10;
+
+        image_clicked = image.getScaledInstance(width + 20,  height + 20, Image.SCALE_DEFAULT);
 
         addMouseListener(new MouseAdapter() {
             @Override
@@ -20,9 +33,30 @@ public class BlackHole extends Entity {
                 System.out.printf("Mouse: (%d, %d)\n", mouseX, mouseY);
 
                 if (mouseX >= x && mouseX <= x + width && mouseY >= y && mouseY <= y + height) {
+                    resizeWhenClicked();
                     player.addCurrency(1);
                 }
             }
         });
     } 
+
+    public void resizeWhenClicked () {
+        this.displayed_image = this.image_clicked;
+        this.displayed_x = this.x_clicked;
+        this.displayed_y = this.y_clicked;
+        repaint();
+
+        ActionListener task = new ActionListener() {
+            public void actionPerformed (ActionEvent evt) {
+                displayed_image = image;
+                displayed_x = x;
+                displayed_y = y;
+                repaint();
+            }
+        };
+
+        Timer timer = new Timer(50, task);
+        timer.setRepeats(false);
+        timer.start();
+    }
 }
--- Engine/Entity.java
+++ Engine/Entity.java
@@ -2,34 +2,42 @@ package Engine;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.image.BufferedImage;
+import java.awt.Dimension;
 import java.io.*;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 
 public class Entity extends JPanel {
     protected Image image;
-    protected int x, y, width, height;
+    protected Image displayed_image;
+    protected int displayed_x, displayed_y, x, y, width, height;
 
     public Entity(String imagePath, int x, int y, int width, int height) {
         this.x = x;
+        this.displayed_x = x;
         this.y = y;
+        this.displayed_y = y;
         this.width = width;
         this.height = height;
 
         try {
             BufferedImage buffered_image = ImageIO.read(new File(imagePath));
-            image = buffered_image.getScaledInstance(width, height, Image.SCALE_DEFAULT);
+            this.image = buffered_image.getScaledInstance(width, height, Image.SCALE_DEFAULT);
+            this.displayed_image = image;
 
         } catch (IOException e) {
             e.printStackTrace();
-        }       
+        }
+
+        this.setSize(new Dimension(x, y));
+
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        if (image != null) {
-            g.drawImage(image, x, y, this);
+        if (this.displayed_image != null) {
+            g.drawImage(this.displayed_image, displayed_x, displayed_y, this);
         }
     }
 }
