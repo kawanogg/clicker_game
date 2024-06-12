66177cc7f8d6a96daac856c96a972ec4153acdf6
Vinicius <vinicius.kmoraes@hotmail.com>
2024-06-12 18:51:13 -0300

    avisa o usuário do erro

--- Engine/ImageNotFoundException.java
+++ Engine/ImageNotFoundException.java
@@ -3,16 +3,23 @@ package Engine;
 import java.io.File;
 import java.io.IOException;
 
+import javax.swing.JOptionPane;
+
 public class ImageNotFoundException extends IOException {
     private String path;
 
     public ImageNotFoundException (String path) {
         super();
         this.path = path;
+        showErrorDialog();
     }
 
     @Override
     public String toString () {
         return "Imagem não encontrada caminho providenciado: " + this.path + ".";
+    }
+
+    private void showErrorDialog() {
+        JOptionPane.showMessageDialog(null, "Não foi possível inicializar o jogo.", "Erro", JOptionPane.ERROR_MESSAGE);
     }
 }
